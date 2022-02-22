e20c8ee7f6a438f87dbecff9dff48a3f7f8ff953
Victor Couto <victor.vdt1@gmail.com>
2022-02-22 10:48:03 -0300

    add components and styles

---
+++ src/components/MainMenu.js
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom"
+import { MainMenuContainer } from "../styles/MainMenuContainer"
+
+export const MainMenu = () => (
+    <MainMenuContainer>
+        <ul>
+            <li>Home</li>
+            <li>Sobre</li>
+        </ul>
+    </MainMenuContainer>
+);
